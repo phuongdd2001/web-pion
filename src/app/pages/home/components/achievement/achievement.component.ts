faaa4430e84adfcab1361b617a94494fd8e86d7f
phuongdd2k1 <phuongdd2001@gmail.com>
2024-08-04 20:45:24 +0700

    Initial commit

--- src/app/pages/home/components/achievement/achievement.component.ts
+++ src/app/pages/home/components/achievement/achievement.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-achievement',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './achievement.component.html',
   styleUrl: './achievement.component.scss'
 })
-export class AchievementComponent {
+export class AchievementComponent implements OnInit {
   achievement: { icon: string, title: string, des: string }[] = [
     { title: 'Total mobile app downloads', des: '100M+', icon: 'assets/images/pages/home/ic_download.svg' },
     { title: 'Monthly Active Users', des: '100M+', icon: 'assets/images/pages/home/ic_user.svg' },
     { title: 'Total of products released', des: '100M+', icon: 'assets/images/pages/home/ic_total_apps.svg' },
   ]
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translate.setDefaultLang(
+    'vi'
+    );
+}
 }
